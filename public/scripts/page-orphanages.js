2742031bf06bf2d3ce531f0152152f34879eb18d
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-10-14 19:27:59 -0300

    começei a configurar o popup da page-orphanages.js

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,5 +1,25 @@
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//create map
+const map = L.map('mapid').setView([-5.4956612,-47.4943441], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+//create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1 class="choose-orphanage"> <img src="./public/images/arrow-write.svg"></a>')
+
+//create and add marker
+L.marker([-5.4956612,-47.4943441], { icon})
+    .addTo(map)
+    .bindPopup(popup)
