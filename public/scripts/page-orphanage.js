f2f08e5d84c3ec8bbc15a2e7db00b2e02958476c
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-10-14 21:23:06 -0300

    removi algumas funcionalidades do mapa dentro da pagina

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,1 +1,26 @@
-orphanage-details-content
+const options = {
+    draggin: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map('mapid',options).setView([-5.4956612,-47.4943441], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create and add marker
+L.marker([-5.4956612,-47.4943441], { icon})
+    .addTo(map)
+    
