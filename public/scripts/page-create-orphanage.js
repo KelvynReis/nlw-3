747ebb2d211366bf7b49336c40c209f3e58630a0
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-10-15 22:26:56 -0300

    começei a desenvolver o mapa do formulario

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,18 @@
+//create map
+const map = L.map('mapid').setView([-5.4956612,-47.4943441], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create and add marker
+L.marker([-5.4956612,-47.4943441], { icon})
+    .addTo(map)
+    .bindPopup(popup)
