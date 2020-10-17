fa29faf8887afb8342a2cede526484f5394bd2c1
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-10-17 19:38:39 -0300

    manipulei o banco de dados test.js

--- src/database/test.js
+++ src/database/test.js
@@ -1,30 +1,22 @@
 const Database = require('./db')
+const saveOrphanage = require('./saveOrphanage')
 
 Database.then(async db=> {
     // inserir dados na tabela
-   await db.run(`
-        INSERT INTO orphanages (
-            lat,
-            lng,
-            name,
-            about,
-            whatsapp,
-            images,
-            instructions,
-            opening_hours,
-            open_on_weekends
-        ) VALUES (
-            "-5.4956612",
-            "-47.4043441",
-            "lar dos meninos",
-            "Presta assistência a crianças de 06 a 15 anos que se encontra em situação de risco e/ou vulnerabilidade social",
-            "99981817618",
+   await saveOrphanage(db,{
+        lat: "-5.4956612",
+        lng: "-47.4043441",
+        name: "lar dos meninos",
+        about: "Presta assistência a crianças de 06 a 15 anos que se encontra em situação de risco e/ou vulnerabilidade social.",
+        whatsapp: "9818177618",
+        images: [
             "https://images.unsplash.com/photo-1600818272779-cfa6145222f0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
-            "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
-            "Horário de visitas Das 18h até 8h",
-            "0"
-        );
-    `)
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ].toString(),
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "o"
+   })
 
     // consultar dados na tabela
     const selectedOrphanages = await db.all("SELECT * FROM orphanages")
@@ -33,4 +25,9 @@ Database.then(async db=> {
     //consultat somente 1 orfanato, pelo id
     const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "1"')
     console.log(orphanage)
+
+    //deletar dado da tabela
+
+   // console.log(await db.run("DELETE FROM orphanages WHERE id ='1'"))
+
 })
