196b499f2c11dad23f081ab7ac2e484ed34d4fa9
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-10-16 22:18:49 -0300

    retirei .html dos link

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -17,7 +17,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1 class="choose-orphanage"> <img src="/images/arrow-white.svg"></a>')
+}).setContent('Lar das meninas <a href="/orphanage?id=1 class="choose-orphanage"> <img src="/images/arrow-white.svg"></a>')
 
 //create and add marker
 L.marker([-5.4956612,-47.4943441], { icon})
