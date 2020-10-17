91902d32b22c9d1f10d65b87369336a96941e152
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-10-16 21:01:06 -0300

    create rota do servidor

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log("ola mundo");
+//inportar dependencia
+const express = require('express');
+//iniciando o express
+const server = express()
+
+//criar uma rota
+server.get('/', () =>{
+    console.log("oi");
+})
+
+//ligar o servidor
+server.listen(5500)
