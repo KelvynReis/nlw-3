c3cb62cb10499ddd8c5daf4e6cb70978328cff91
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-10-18 20:59:56 -0300

    terceiro commit

--- src/pages.js
+++ src/pages.js
@@ -8,8 +8,30 @@ module.exports = {
         return res.render('index')
     },
 
-    orphanage(req,res){
-        return res.render('orphanage')
+    async orphanage(req,res){
+
+        const id = req.query.id
+
+        try {
+            const db = await Database;
+            const results = await db.all(`SELECT * FROM orphanages WHERE id = ${id}`)
+            const orphanage = results[0]
+            
+            orphanage.images = orphanage.images.split(",")
+            orphanage.firstImage = orphanage.images[0]
+
+            if(orphanage.open_on_weekends == "1"){
+                orphanage.open_on_weekends = false
+            }else{
+                orphanage.open_on_weekends = true
+            }
+
+            return res.render('orphanage', { orphanage })
+        } catch (error) {
+            console.log(error);
+            return res.send('Erro no banco de dados')
+
+        }
     },
 
     async orphanages(req,res){
