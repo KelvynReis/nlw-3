25cb9cf965dd3f7d4e690261c351b66e48bc68e0
Kelvyn Reis <kelvyn2reis@gmail.com>
2020-10-18 21:26:59 -0300

    get values from html

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -5,10 +5,12 @@ const options = {
     scrollWheelZoom: false,
     zoomControl: false
 }
-
+// get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat
+const lng = document.querySelector('span[data-lng]').dataset.lng
 
 //create map
-const map = L.map('mapid',options).setView([-5.4956612,-47.4943441], 15);
+const map = L.map('mapid',options).setView([lat,lng], 15);
 
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -22,7 +24,10 @@ const icon = L.icon({
 })
 
 //create and add marker
-L.marker([-5.4956612,-47.4943441], { icon}).addTo(map)
+
+L
+.marker([lat, lng], { icon})
+.addTo(map)
 
 /* image gallery*/ 
 
